bbe4db6548d024c3c994b8109aa82e0ad0fd14d9
LuisDiegoMora98 <aguilarluisdi@estudiantec.cr>
2022-03-24 13:49:45 -0600

    Component and templte modified, missing logic and fetch from backend

---
+++ FrontEnd/my-app/src/components/movie/LoginComponent.tsx
@@ -0,0 +1,66 @@
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Checkbox,
+    Stack,
+    Link,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
+  } from '@chakra-ui/react';
+import React from 'react';
+  
+  export default function LoginComponent() {
+    return (
+      <Flex
+        minH={'100vh'}
+        align={'center'}
+        justify={'center'}
+        bg={useColorModeValue('gray.50', 'gray.800')}>
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+          <Stack align={'center'}>
+            <Heading fontSize={'4xl'}>Bienvenido a Cinepolis</Heading>
+            <Text fontSize={'lg'} color={'gray.600'}>
+              Compra y reserva tus peliculas y comidas favoritas
+            </Text>
+          </Stack>
+          <Box
+            rounded={'lg'}
+            bg={useColorModeValue('white', 'gray.700')}
+            boxShadow={'lg'}
+            p={8}>
+            <Stack spacing={4}>
+              <FormControl id="email">
+                <FormLabel>Correo Electrónico</FormLabel>
+                <Input type="email" />
+              </FormControl>
+              <FormControl id="password">
+                <FormLabel>Contraseña</FormLabel>
+                <Input type="password" />
+              </FormControl>
+              <Stack spacing={10}>
+                <Stack
+                  direction={{ base: 'column', sm: 'row' }}
+                  align={'start'}
+                  justify={'space-between'}>
+                  <Checkbox>Recuérdame</Checkbox>
+                </Stack>
+                <Button
+                  bg={'blue.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'blue.500',
+                  }}>
+                  Iniciar Sesión
+                </Button>
+              </Stack>
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
+    );
+  }
