5a15f4d63470f6b36d9c87cec1217f89458b8cc5
LuisDiegoMora98 <aguilarluisdi@estudiantec.cr>
2022-03-27 13:16:26 -0600

    Update LoginComponent.tsx

--- FrontEnd/my-app/src/components/user/LoginComponent.tsx
+++ FrontEnd/my-app/src/components/user/LoginComponent.tsx
@@ -1,83 +1,113 @@
 import {
-    Flex,
-    Box,
-    FormControl,
-    FormLabel,
-    Input,
-    Checkbox,
-    Stack,
-    Link,
-    Button,
-    Heading,
-    Text,
-    useColorModeValue,
-  } from '@chakra-ui/react';
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Link,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import axios from 'axios';
 import React, { useState } from 'react';
-  
-  export default function LoginComponent() {
-    const [email, setEmail] = useState("");
-    const handleEmailInput = (e : any) => {
-      setEmail(e.target.value);
-    }
-    const [pass, setPass] = useState("");
-    const handlePassInput = (e : any) => {
-      setPass(e.target.value);
-    }
+import { useHistory } from 'react-router-dom';
 
-    const handleSubmit = () => {
-      //check if user exists on database, then user type and re route to corresponding user type home page
-      console.log(email);
-      console.log(pass);
+export default function LoginComponent() {
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const handleEmailInput = (e : any) => {
+    setEmail(e.target.value);
+  }
+  const [pass, setPass] = useState("");
+  const handlePassInput = (e : any) => {
+    setPass(e.target.value);
+  }
+
+  const handleSubmit = () => {
+    handleLogin();
+  }
+
+  const handleLogin = async () => {
+    //check if user exists on database, then user type and re route to corresponding user type home page
+    //this should be in a controller that calls a provider, but whtvs
+    if(email && pass){
+      const body = {
+        email,
+        pass
+      } 
+      axios
+          .post("http://localhost:5000/api/users/login", body)
+          .then((response : any)=>{
+             switch (response.data.type) {
+                case "Admin":
+                  history.push("/moviesAdmin");
+                  break;
+                case "Client":
+                  history.push("/movies");
+                  break;
+                default:
+                  alert("Usuario invalido");
+                  break;
+             }
+          })
+          .catch((err)=>{
+            console.log("Err ", err);
+            alert(err);
+          })
       
     }
-
-    return (
-      <Flex
-        minH={'100vh'}
-        align={'center'}
-        justify={'center'}
-        bg={useColorModeValue('gray.50', 'gray.800')}>
-        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-          <Stack align={'center'}>
-            <Heading fontSize={'4xl'}>Bienvenido a Cinepolis</Heading>
-            <Text fontSize={'lg'} color={'gray.600'}>
-              Compra y reserva tus peliculas y comidas favoritas
-            </Text>
-          </Stack>
-          <Box
-            rounded={'lg'}
-            bg={useColorModeValue('white', 'gray.700')}
-            boxShadow={'lg'}
-            p={8}>
-            <Stack spacing={4}>
-              <FormControl id="email">
-                <FormLabel>Correo Electrónico</FormLabel>
-                <Input value={email} onChange={handleEmailInput} type="email" />
-              </FormControl>
-              <FormControl id="password">
-                <FormLabel>Contraseña</FormLabel>
-                <Input value={pass} onChange={handlePassInput} type="password" />
-              </FormControl>
-              <Stack spacing={10}>
-                <Stack
-                  direction={{ base: 'column', sm: 'row' }}
-                  align={'start'}
-                  justify={'space-between'}>
-                  <Checkbox>Recuérdame</Checkbox>
-                </Stack>
-                <Button
-                onClick={handleSubmit}
-                  bg={'blue.400'}
-                  color={'white'}
-                  _hover={{
-                    bg: 'blue.500',
-                  }}>
-                  Iniciar Sesión
-                </Button>
-              </Stack>
-            </Stack>
-          </Box>
-        </Stack>
-      </Flex>
-    );
   }
+  return (
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'}>Bienvenido a Cinepolis</Heading>
+          <Text fontSize={'lg'} color={'gray.600'}>
+            Compra y reserva tus peliculas y comidas favoritas
+          </Text>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={4}>
+            <FormControl id="email">
+              <FormLabel>Correo Electrónico</FormLabel>
+              <Input value={email} onChange={handleEmailInput} type="email" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Contraseña</FormLabel>
+              <Input value={pass} onChange={handlePassInput} type="password" />
+            </FormControl>
+            <Stack spacing={10}>
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                align={'start'}
+                justify={'space-between'}>
+                <Checkbox>Recuérdame</Checkbox>
+              </Stack>
+              <Button
+              onClick={handleSubmit}
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}>
+                Iniciar Sesión
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+}
