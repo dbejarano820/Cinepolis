464a564f3b6750fd4893bf4627101d23380144ef
LuisDiegoMora98 <aguilarluisdi@estudiantec.cr>
2022-03-27 13:18:02 -0600

    Email sending + login, still gotta fix update pass

--- BackEnd/routes/routes.ts
+++ BackEnd/routes/routes.ts
@@ -1,7 +1,7 @@
 import * as express from "express";
 import {movierouter} from './movierouter';
 import {foodrouter} from './foodrouter';
-//import { userrouter } from "./userrouter";
+import { userrouter } from "./userrouter";
 
 class Routes {
 
@@ -22,8 +22,7 @@ class Routes {
     private routes(): void {
         this.express.use('/movies', movierouter);
         this.express.use('/food', foodrouter);
-        //this.express.use('/user', userrouter);
-        //this.express.use('/users', userrouter);
+        this.express.use('/users', userrouter);
     }
 }
 
--- FrontEnd/my-app/src/App.tsx
+++ FrontEnd/my-app/src/App.tsx
@@ -1,13 +1,17 @@
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import React from "react";
+
 import SidebarWithHeader from "./components/sections/header";
 import MovieListing from "./components/movie/MovieListing";
 import MovieDetails from "./components/movie/MovieDetails";
 import FoodList from "./components/food/FoodList"
+
 import FoodDetail from "./components/food/FoodDetail" 
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import React from "react";
+import AddFood from "./components/food/AddFood";
+
 import LoginComponent from "./components/user/LoginComponent";
 import SignUpComponent from "./components/user/SignUpComponent";
-import AddFood from "./components/food/AddFood";
+import ConfirmPassComponent from "./components/user/ConfirmPassComponent";
 // import SidebarwithHeader from "./components/sections/headerr"
    
 function App() {
@@ -19,6 +23,7 @@ function App() {
        <Switch>
           <Route exact path="/" component={LoginComponent} />
           <Route exact path="/signUp" component={SignUpComponent} />
+          {/* <Route exact path="/updatePassword/:token" component={ConfirmPassComponent} /> */}
           <Route exact path="/movie/:movieId" component={MovieDetails} />
           <Route exact path="/food" component={FoodList} />
           <Route exact path="/food/:name" component={FoodDetail} />  
