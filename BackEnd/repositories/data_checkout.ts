cde2484f66fef6557abf1a33e13d94583955bd84
Daniel Bejarano <dbejarano820@gmail.com>
2022-03-30 14:36:10 -0600

    Tweaks +

--- BackEnd/repositories/data_checkout.ts
+++ BackEnd/repositories/data_checkout.ts
@@ -12,22 +12,23 @@ export class checkout_data {
     public async payment(info : any) {
         console.log("Info Payment query");
         console.log(info);
+        await this.db.query("select create_reservation($1);", [info.toAddress]);
         let statement = "";
         let values : any = {};
-        info.products.forEach((product : any) => {
+        info.products.forEach(async (product : any) => {
             if(product.type === "Ticket"){
-                statement = "reserve_seat($1, %2, $3, $4, %5, %6, %7);";
-                values = [info.toAddress, product.row, product.number, product.type,
+                statement = "select reserve_seat($1, $2, $3, $4, $5, $6, $7);";
+                values = [info.toAddress, product.row, product.num, product.type,
                             product.movie, product.time, product.sala];
             }
             if(product.type === "Food"){
-                statement = "reserve_food($1, %2);";
+                statement = "select reserve_food($1, $2);";
                 values = [info.toAddress, product.food_name];
             }
+            await this.db.query(statement, values);
         });
-        await this.db.query("create_reservation();");
-        await this.db.query(statement, values);
-        await this.db.query("deactive_reservation();");
+        
+        await this.db.query("select deactive_reservation($1);", [info.toAddress]);
     } 
 }
 
